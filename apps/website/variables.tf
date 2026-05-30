7b2603437c3d0d04f7d5961b4af1bfed12982b60
Somasundar S <somasundar.dev@gmail.com>
2026-05-31 02:18:24 +0530

    feat: generate release and documentation

--- apps/website/variables.tf
+++ apps/website/variables.tf
@@ -1,4 +1,15 @@
-variable "project_name" { type = string }
-variable "environment" { type = string }
-variable "region" { type = string }
-variable "domain_name" { type = string }
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
