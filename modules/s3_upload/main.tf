3acd33567df91b8ce049e40d59d4b74ac6546e17
Somasundar S <somasundar.dev@gmail.com>
2026-05-31 01:39:35 +0530

    chore: remove terraform

--- modules/s3_upload/main.tf
+++ modules/s3_upload/main.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_version = ">= 1.3"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+provider "aws" {
+  region = var.region
 }
