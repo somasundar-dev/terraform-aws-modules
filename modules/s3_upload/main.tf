b690b17a157db6a88840e0a8f60afc32a6461eea
Somasundar S <somasundar.dev@gmail.com>
2026-05-31 01:41:25 +0530

    chore: remove provider

--- modules/s3_upload/main.tf
+++ modules/s3_upload/main.tf
@@ -1,3 +1,10 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
